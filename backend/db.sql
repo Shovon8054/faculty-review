f43234f969db32dc9fbdb67f9d0a3d7c5edfe45f
Mehedi Hasan Shovon <mehedi.shovon07@gmail.com>
2026-06-21 00:36:46 +0600

    Update db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -101,9 +101,7 @@ CREATE TABLE queries (
 ) ENGINE=InnoDB;
 
 -- =========================
--- SEED SUPER ADMIN (hash password in Node and paste here)
--- email: admin@bracu.ac.bd
--- pass: superadmin123
+-- email: admin@bracu.ac.bd   pass : Admin1234
 -- =========================
 INSERT INTO users (name, email, password_hash, role)
 VALUES ('Super Admin', 'admin@bracu.ac.bd', '$2b$10$REPLACE_WITH_BCRYPT_HASH', 'admin');
